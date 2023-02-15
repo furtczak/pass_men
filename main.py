4932390dd53bed70e713e6f82bbdf513ef0b0853
MrRazeLL <frytka202020@gmail.com>
2023-02-15 17:42:43 +0100

    pass_men  A1

---
+++ main.py
@@ -0,0 +1,18 @@
+m_pwd=input("Main passwrd: ")
+
+def view():
+    pass
+
+def add():
+    pass
+
+while True:
+    mode =input("What would you want to do, add new pass or view existing ones (v=view,a=add,q=quit)")
+    if mode=="q":
+        break
+    if mode=="a":
+        pass
+    elif mode =="v":
+        pass
+    else:
+        print("invalid choice")
