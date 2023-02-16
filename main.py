77757ca2603677c1dc6526e31252361c715f5f61
MrRazeLL <frytka202020@gmail.com>
2023-02-16 15:05:49 +0100

    gotowe

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
+import os
+
 m_pwd=input("Main passwrd: ")
 
 def view():
-    pass
-
+    with open('pass.txt',"r") as f:
+        for line in f.readlines():
+            data=(line.rstrip())
+            user,pas=data.split("|")
+            print(user, pas)
 def add():
-    pass
+    name=input('Name: ')
+    pas=input('Password: ')
+     
+    with open('pass.txt','a')as f:
+        f.write(name+"|"+pas+"\n")
+pass_file='D:\VSC pliki\pass_men\pass_men\pass.txt'
+
+def remove(l_num):
+    newpath=pass_file+".new"
+    with open(pass_file,'r') as file, open(newpath, 'w') as new_file:
+        for Line_num, line in enumerate(file):
+            if Line_num != l_num:
+                new_file.write(line)
+
+    os.replace(newpath,pass_file)
 
 while True:
-    mode =input("What would you want to do, add new pass or view existing ones (v=view,a=add,q=quit)")
+    mode =input("What would you want to do, add new pass or view existing ones (v=view,a=add,q=quit,r=remove): ")
     if mode=="q":
         break
     if mode=="a":
-        pass
+        add()
     elif mode =="v":
-        pass
+        view()
+    elif mode=="r":
+        num=input("pleas specify a line: ")
+        if num!=0:
+            remove(int(num))
     else:
         print("invalid choice")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,18 +1,24 @@
 m_pwd=input("Main passwrd: ")
 
 def view():
-    pass
-
+    with open('pass.txt',"r") as f:
+        for line in f.readlines():
+            print(line.rstrip())
+            
 def add():
-    pass
+    name=input('Name: ')
+    pas=input('Password: ')
+     
+    with open('pass.txt','a')as f:
+        f.write(name+"|"+pas+"\n")
 
 while True:
-    mode =input("What would you want to do, add new pass or view existing ones (v=view,a=add,q=quit)")
+    mode =input("What would you want to do, add new pass or view existing ones (v=view,a=add,q=quit): ")
     if mode=="q":
         break
     if mode=="a":
-        pass
+        add()
     elif mode =="v":
-        pass
+        view()
     else:
         print("invalid choice")
